68b224106491820a8b8d54f18c183c2dcbf3de21
Usman-coder21 <usmanrafique078@gmail.com>
2025-03-10 03:52:43 +0500

    update

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,8 +1,43 @@
-export const signup= (req,res)=>{
+import User from '../models/user.model.js'
+import { generateToken } from '../lib/utils.js';
+import bcrypt from 'bcryptjs'
+
+export const signup= async (req,res)=>{
     const{fullName,email,password}=req.body;
     try{
+        if(!fullName || !email || !password){
+            return res.status(400).json({message:"All fields are required"});
+        }
+        if(password.length < 8){
+            return res.status(400).json({message:"Password must be atleast 8 characters long"});
+        }
+const user= await User.findOne({email})
+if(user)
+    return res.status(400).json({message:"User already exists"});
+const salt= await bcrypt.genSalt(10);
+const hashedPassword= await bcrypt.hash(password,salt);
+const newUser= new User({
+    fullName,
+    email,
+    password:hashedPassword});
+    if(newUser){
+        generateToken(newUser._id,res);
+        await newUser.save();
+        return res.status(201).json({
+            _id:newUser._id,
+            fullName:newUser.fullName,
+            email:newUser.email,
+            profilePic:newUser.profilePic
+        });
+    }else{
+        return res.status(400).json({message:"Failed to create user"});
 
-    }catch(error){}
+    }
+ } catch(error){
+    console.log("error in signup",error.message);
+    return res.status(500).json({message:"Internal Server Error"});
+    
+ }
 }
 
 export const login= (req,res)=>{
