918185612dc0a1b2c3247430f7e22175299bbffa
Usman-coder21 <usmanrafique078@gmail.com>
2025-03-12 04:11:40 +0500

    update

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -3,6 +3,7 @@ import { useAuthStore  } from '../store/useAuthStore'
 import { Eye, EyeOff, Loader2, Mail, MessageSquare, User } from 'lucide-react'
 import { Link } from 'react-router-dom'
 import AuthImagePattern from '../components/AuthImagePattern.jsx'
+import toast from 'react-hot-toast'
 
 const SignUpPage = () => {
   const [showPassword, setShowPassword] = useState(false);
